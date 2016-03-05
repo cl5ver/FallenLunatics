a7c6a6b5cb72de0da33c0bc521852f8107f612db
clover3leaf <clover3leaf@gmail.com>
2016-03-05 15:20:07 +0900

    ImagePanel some java imports removed

--- FLPhoto/src/fallen/lunatics/photo/ImagePanel.java
+++ FLPhoto/src/fallen/lunatics/photo/ImagePanel.java
@@ -3,10 +3,7 @@ package fallen.lunatics.photo;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
